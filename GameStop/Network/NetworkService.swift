5453cee8ff6df3389246beeb6840faa48984d095
Güray Gül <guraygul59@gmail.com>
2024-05-19 22:21:58 +0300

    Added retry for fetch method

--- GameStop/Network/NetworkService.swift
+++ GameStop/Network/NetworkService.swift
@@ -18,17 +18,24 @@ final class NetworkService: NetworkServiceProtocol {
             throw NetworkError.invalidURL
         }
         
-        let (data, response) = try await URLSession.shared.data(from: url)
-        
-        guard let httpResponse = response as? HTTPURLResponse, (200...299).contains(httpResponse.statusCode) else {
-            throw NetworkError.invalidResponse
-        }
-        
         do {
-            let decodedData = try JSONDecoder().decode(T.self, from: data)
-            return decodedData
+            let (data, response) = try await URLSession.shared.data(from: url)
+            
+            if let httpResponse = response as? HTTPURLResponse {
+                if !(200...299).contains(httpResponse.statusCode) {
+                    throw NetworkError.invalidResponse
+                }
+            }
+            
+            do {
+                let decodedData = try JSONDecoder().decode(T.self, from: data)
+                return decodedData
+            } catch {
+                throw NetworkError.decodingError(error)
+            }
         } catch {
-            throw NetworkError.decodingError(error)
+            throw error
         }
     }
+
 }
--- GameStop/Scenes/Home/HomeViewModel.swift
+++ GameStop/Scenes/Home/HomeViewModel.swift
@@ -25,27 +25,35 @@ final class HomeViewModel {
     init(networkService: NetworkServiceProtocol = NetworkService.shared) {
         self.networkService = networkService
     }
-    
-    private func fetchGames() {
+        
+    private func fetchGames(retryCount: Int = 0) {
         Task {
             do {
                 let gameModel = try await networkService.fetchData(from: GameAPI.games(page: 1),
-                                                                   as: GameModel.self
-                )
+                                                                   as: GameModel.self)
                 self.games = gameModel.results ?? []
                 DispatchQueue.main.async { [weak self] in
                     guard let self = self else { return }
                     self.view?.reloadData()
                 }
             } catch {
-                DispatchQueue.main.async { [weak self] in
-                    guard let self = self else { return }
+                if retryCount < 3 {
+                    let delay = pow(2.0, Double(retryCount))
                     view?.showAlert(title: "Error fetching games",
-                                    message: error.localizedDescription)
+                                         message: "Retrying in \(delay) seconds...")
+                    try await Task.sleep(nanoseconds: UInt64(delay * 1_000_000_000))
+                    fetchGames(retryCount: retryCount + 1)
+                } else {
+                    DispatchQueue.main.async { [weak self] in
+                        guard let self = self else { return }
+                        self.view?.showAlert(title: "Error fetching games",
+                                             message: error.localizedDescription)
+                    }
                 }
             }
         }
     }
+
 }
 
 extension HomeViewModel: HomeViewModelInterface {
